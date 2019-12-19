0a889bdf0123c49e118962021656a832843f9749
Itachi <samcravatta@gmail.com>
2019-12-19 01:44:28 -0600

    Initial Commit

---
+++ gitty/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  gitty
+//
+//  Created by Sam Cravatta on 12/19/19.
+//  Copyright © 2019 Leaf_Programming. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
